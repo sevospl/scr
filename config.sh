ff9feb403c002d4e8851f80c8d34d993f5eb9fea
Hitesh Thawani <hiteshthawani1995@gmail.com>
2018-06-17 21:44:42 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -41,7 +41,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "      LineageOS Recorder       "
   ui_print "*******************************"
 }
 
